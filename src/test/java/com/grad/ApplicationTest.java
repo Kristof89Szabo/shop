b162bf68ed863f485119df8dbce5636156dd5f3c
Kristof89Szabo <kristof89szabo@gmail.com>
2023-04-06 14:57:05 +0200

    Create ci.yml (#4)
    * Create ci.yml

--- src/test/java/com/grad/ApplicationTest.java
+++ src/test/java/com/grad/ApplicationTest.java
@@ -7,11 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@MicronautTest
 class ApplicationTest {
-
-    @Inject
-    EmbeddedApplication<?> application;
 
     @Test
     void testItWorks() {
